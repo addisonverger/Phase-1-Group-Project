24bcd2c56122350dc73bce7fb6cbe7559b7a9d0c
Becca Randall <becca.m.randall@gmail.com>
2018-11-19 20:43:43 -0600

    WIP on ScrollMagic

--- index.js
+++ index.js
@@ -23,3 +23,85 @@ function init () {
 }
 
 document.addEventListener('DOMContentLoaded', init)
+// document.addEventListener("DOMContentLoaded", init)
+
+// function init () {
+//     activateScrollMagic()
+// }
+
+// function activateScrollMagic () {
+    //Init ScrollMagic Controller
+    var controller = new ScrollMagic.Controller();
+    // Define ScrollMagic Scene
+    var containerScene = new ScrollMagic.Scene({
+        triggerElement: '.dynamicContent #loader',
+        triggerHook: "onEnter"
+    })
+    .addIndicators()
+    .addTo(controller)
+    .on("enter", function (event) {
+        if (!$("#loader").hasClass("active")) {
+            $("#loader").addClass("active");
+            if (console){
+                console.log("loading new items");
+            }
+            setTimeout(addResults, 1000, 2);
+        }
+    })
+    // };
+
+function addResults (amount) {
+    for (i=1; i<=amount; i++) {
+        // var randomColor = '#'+('00000'+(Math.random()*0xFFFFFF<<0).toString(16)).slice(-6);
+        $("<div>Test</div>")
+            .addClass("tile is-dark notification is-child box")
+            .css({
+                "height": "+=100",
+                "width": "+=100",
+                "background-color": "hsl(0, 0%, 96%)",
+            })
+            .appendTo(".dynamicContent #content");
+    }
+    // "loading" done -> revert to normal state
+    containerScene.update(); // make sure the scene gets the new start position
+    $("#loader").removeClass("active");
+    }
+
+addResults(3)
+
+// <div class="tile is-dark notification is-child box">
+//         <h2 class="title is-4">Ariana Grande</h2>
+//         <div class="columns">
+//           <div class="column">
+//               <h3 class="title is-5 has-text-info">Artist Bio</h3>
+//               <p>Ariana Grande-Butera (; born June 26, 1993) is an American singer, songwriter and actress. She began
+//                 her career in 2008 in the Broadway musical 13, before playing the role of Cat Valentine in the Nickelodeon
+//                 television series Victorious (2010–2013) and in the spinoff Sam & Cat (2013–2014). She has also appeared in
+//                 other theatre and television roles and has lent her voice to animated television and films. <a href="">(read more)</a></p>
+//           </div>
+//           <div class="column">
+//               <iframe width="560" height="315" src="https://www.youtube.com/embed/ffxKSjUwKdU" frameborder="0"
+//               allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+//           </div>
+//         </div>
+//         <h3 class="title is-5 has-text-info">Upcoming Shows</h3>
+//         <div class="tile is-ancestor">
+//           <div class="tile is-parent">
+//             <div class="tile is-child box button is-info is-inverted is-outlined">
+//               <p>May 19<br />Toyota Center<br />Houston, TX</p>
+//             </div>
+//           </div>
+//           <div class="tile is-parent">
+//             <div class="tile is-child box button is-info is-inverted is-outlined">
+//               <p>May 17<br />AT&T Center<br />San Antonio, TX</p>
+//             </div>
+//           </div>
+//           <div class="tile is-parent">
+//             <div class="tile is-child box button is-info is-inverted is-outlined">
+//               <p>May 21<br />American Airlines Center<br />Dallas, TX</p>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
