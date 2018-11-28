5f8be49e6d41d167c5a0a7e1bbdd971e356cda72
Joseph Madamba <jpmadamba@gmail.com>
2018-11-27 18:39:24 -0600

    TasteDive API call

--- index.js
+++ index.js
@@ -18,41 +18,6 @@ function getRelatedArtistEvents () {
     })
 }
 
-<<<<<<< HEAD
-
-
-
-
-var apiCall = 'https://tastedive.com/api/similar?q=pearl%20jam&type=music&info=1&k=323666-showGo-XUMS94RP';
-
-
-
-let h = new Headers();
-h.append('Accept', 'application/json');
-let req = new Request(apiCall,{
-    method: 'GET',
-    headers: h,
-    mode: 'cors'
-});
-
-
-
-fetch(req)
-    .then( (response)=>{
-      if(response.ok){
-          return response.json();
-      }else{
-          throw new Error('Fetch Error');
-      }
-      
-    })
-    .then( (jsonData)=>{
-        console.log(jsonData);
-    })
-    .catch( (err)=>{
-        console.log('ERROR:', err.message);
-    });
-=======
 function init () {
     document.getElementsByClassName('button')[0].addEventListener('click', getRelatedArtistEvents)
 }
@@ -101,4 +66,3 @@ function addResults (amount) {
 
 // Set the initial number of results to appear on the page
 addResults(5)
->>>>>>> 1f34f424e951caa7389e97668f18443410091b02
