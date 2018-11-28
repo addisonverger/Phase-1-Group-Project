c21e1f314db5d1073f533f92fed0c8368db5db3b
Joseph Madamba <jpmadamba@gmail.com>
2018-11-27 20:57:57 -0600

    "fixed CORS error"

--- index.js
+++ index.js
@@ -29,31 +29,18 @@ var infoType = '&type=music&info=1';
 var apiKey = '&k=323666-showGo-XUMS94RP';
 var apiCall = tastedive + artistQuery + infoType + apiKey;
 
-let h = new Headers();
-h.append('Accept', 'application/json');
-let req = new Request(apiCall,{
-    method: 'GET',
-    headers: h,
-    mode: 'cors'
-});
+$.ajax({
+    url : apiCall,
+    method : 'GET',
+    dataType : 'jsonp',
+    
 
 
+})
+.then (function (response){
+    console.log(response)
+})
 
-fetch(req)
-    .then( (response)=>{
-      if(response.ok){
-          return response.json();
-      }else{
-          throw new Error('Fetch Error');
-      }
-      
-    })
-    .then( (jsonData)=>{
-        console.log(jsonData);
-    })
-    .catch( (err)=>{
-        console.log('ERROR:', err.message);
-    });
 
 // end TasteDive
 
