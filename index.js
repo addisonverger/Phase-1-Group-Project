9ad9f2a71c48b75b2659c7e3ae6a268b80280464
RenteriaL <renterial@thesharepointprofessional.com>
2018-12-02 16:10:34 -0600

    pushing latest

--- index.js
+++ index.js
@@ -1,96 +1,55 @@
+// Search-bar click to pass value to API calls and render results and init ScrollMagic and Accordian
+document.getElementById("search").addEventListener('click', function(){
+  callTastedive ()
+  var results = document.getElementById("results-container")
+  results.innerHTML = renderResultsCard
+  renderResultsCard()
 
-function getRelatedArtistEvents () {
-  var URL = 'http://rest.bandsintown.com/artists/rittz/events?app_id=c283929e0751cf243b17ca899c564814'
-  $.ajax({
-    url: URL,
-    method: 'GET',
-    dataType: 'jsonp',
-    error: function (reason, xhr) {
-      console.log('error in processing your request', reason)
-    }
-  })
-    .then(function (response) {
-      response.forEach(element => {
-        $('#test-container').append(`
-                    <div>${element.venue.name}</div>
-                    <div>${element.venue.city}</div>
-                    <div>${element.venue.region}</div>`)
-      })
+    // Init ScrollMagic Controller
+
+    var controller = new ScrollMagic.Controller()
+    // Define ScrollMagic Scene
+    var containerScene = new ScrollMagic.Scene({
+      triggerElement: '.container #loader',
+      triggerHook: 'onEnter'
     })
-}
-
-var apiCall = 'https://tastedive.com/api/similar?q=pearl%20jam&type=music&info=1&k=323666-showGo-XUMS94RP'
-
-/// TasteDive API call
-
-var tastedive = 'https://tastedive.com/api/similar?q='
-var artistQuery = 'david bowie'
-var infoType = '&type=music&info=1'
-var apiKey = '&k=323666-showGo-XUMS94RP'
-var apiCall = tastedive + artistQuery + infoType + apiKey
-
-$.ajax({
-  url: apiCall,
-  method: 'GET',
-  dataType: 'jsonp'
-
-})
-  .then(function (response) {
-    console.log(response)
-  })
-
-// end TasteDive
-
-function init () {
-  document.getElementsByClassName('button')[0].addEventListener('click', getRelatedArtistEvents)
-}
-
-document.addEventListener('DOMContentLoaded', init)
-
-// Init ScrollMagic Controller
-var controller = new ScrollMagic.Controller()
-// Define ScrollMagic Scene
-var containerScene = new ScrollMagic.Scene({
-  triggerElement: '.container #loader',
-  triggerHook: 'onEnter'
-})
-// .addIndicators()
-  .addTo(controller)
-  .on('enter', function (event) {
-    if (!$('#loader').hasClass('active')) {
-      $('#loader').addClass('active')
-      if (console) {
-        console.log('loading new items')
-      }
-      setTimeout(addResults, 1000, 9)
-    }
-  })
-
-// Function to add in new search results
-function addResults (amount) {
-// TO DO: Need to update this for loop so that it adds a result from the search, not hard coded div
-  for (var i = 1; i <= amount; i++) {
-    $('<div>This is where a new result will go</div>')
-      .addClass('tile is-dark notification is-child box')
-      .css({
-        'width': '100%',
-        'background-color': '#4a4a4a',
-        'font-size': '1rem',
-        'font-weight': '400',
-        'line-height': '1.5',
-        'font-family': 'BlinkMacSystemFont,-apple-system,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,Helvetica,Arial,sans-serif'
+    // .addIndicators()
+      .addTo(controller)
+      .on('enter', function (event) {
+        if (!$('#loader').hasClass('active')) {
+          $('#loader').addClass('active')
+          if (console) {
+            console.log('loading new items')
+          }
+          setTimeout(addResults, 1000, 9)
+        }
       })
-      .appendTo('.container #results-container')
-  }
-  // "loading" done -> revert to normal state
-  containerScene.update() // make sure the scene gets the new start position
-  $('#loader').removeClass('active')
-}
+    // Function to add in new search results
+    function addResults (amount) {
+    // TO DO: Need to update this for loop so that it adds a result from the search, not hard coded div
+      for (var i = 1; i <= amount; i++) {
+        $('<div>This is where a new result will go</div>')
+          .addClass('tile is-dark notification is-child box')
+          .css({
+            'width': '100%',
+            'background-color': '#4a4a4a',
+            'font-size': '1rem',
+            'font-weight': '400',
+            'line-height': '1.5',
+            'font-family': 'BlinkMacSystemFont,-apple-system,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,Helvetica,Arial,sans-serif'
+          })
+          .appendTo('.container #results-container')
+      }
+      // "loading" done -> revert to normal state
+      containerScene.update() // make sure the scene gets the new start position
+      $('#loader').removeClass('active')
+    }
+    // Set the initial number of results to appear on the page
+    addResults(5)
 
-// Set the initial number of results to appear on the page
-addResults(5)
+    // end ScrollMagic Controller
 
-/// Accordion Toggle///
+    /// Accordion Toggle
 
 var header = document.getElementsByClassName('tile-header')
 
@@ -120,3 +79,88 @@ function hideAll () {
     header[i].parentNode.removeAttribute('style')
   }
 }
+
+})
+
+/// TasteDive API call
+function callTastedive () {
+var tastedive = 'https://tastedive.com/api/similar?q='
+var artistQuery = document.getElementById("search-bar").value
+var infoType = '&type=music&info=1'
+var apiKey = '&k=323666-showGo-XUMS94RP'
+var makeCall = tastedive + artistQuery + infoType + apiKey
+$.ajax({
+  url: makeCall,
+  method: 'GET',
+  dataType: 'jsonp'
+})
+  .then(function (response) {
+    console.log(response)
+  })
+  // make BandsinTown API Call
+  getRelatedArtistEvents()
+}
+// end TasteDive
+
+// BandsinTown API Call
+
+function getRelatedArtistEvents () {
+  var bandsintown = 'http://rest.bandsintown.com/artists/'
+  var artistQuery = document.getElementById("search-bar").value
+  var infoType = '/events?'
+  var apiKey = 'app_id=c283929e0751cf243b17ca899c564814'
+  var makeCall = bandsintown + artistQuery + infoType + apiKey
+
+  $.ajax({
+    url: makeCall,
+    method: 'GET',
+    dataType: 'jsonp',
+    error: function (reason, xhr) {
+      console.log('error in processing your request', reason)
+    }
+  })
+    .then(function (response) {
+      response.forEach(element => {
+        console.log(response)
+      })
+    })
+  renderResultsCard()
+}
+
+// end BandsinTown
+
+// Render results
+
+function renderResultsCard() {
+  renderUpcomingResults()
+  return `
+    <div class="tile is-dark notification is-child box band-listing">
+    <h2 class="title is-4 tile-header">Name</h2>
+    <div class="tile-body" style="max-height: 0px">
+      <div class="columns">
+        <div class="column">
+            <h3 class="title is-5 has-text-info">Artist Bio</h3>
+            <p>wTeaser</p>
+        </div>
+        <div class="column">
+            <iframe width="560" height="315" src="yUrl" frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+      </div>
+      <h3 class="title is-5 has-text-info">Upcoming Shows</h3>
+      <div class="tile is-ancestor"></div>
+  `
+}
+
+function renderUpcomingResults() {
+  return `
+      <div class="tile is-parent">
+        <div class="tile is-child box button is-info is-inverted is-outlined">
+          <p>datetime<br />venuename<br />venuecity</p>
+        </div>
+      </div>
+  `
+}
+
+//end Render results
+
